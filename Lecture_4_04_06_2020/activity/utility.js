d3ccd0931f2b9389ea51e44df6fdb91ef3f55d0e
Prashuk Jain <47266758+PrashukJain@users.noreply.github.com>
2020-06-13 16:32:09 +0530

    Delete utility.js

--- Lecture_4_04_06_2020/activity/utility.js
+++
@@ -1,6 +0,0 @@
-module.exports.types = {
-    media: ["mp4", "mkv","jpg","jpeg","png"],
-    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
-    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
-    app: ['exe', 'dmg', 'pkg', "deb"]
-}
